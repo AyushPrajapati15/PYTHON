ba5b261e7ec76d707072065d5463992113677862
unknown <aayushprajapati867gmail.com>
2023-04-27 11:28:00 +0530

    Day-1

---
+++ DAY1/code1.py
@@ -0,0 +1,44 @@
+name="Alice"
+print(name)
+print(name.find('s'))
+print(name.replace("Alice","Bob"))
+print(name.replace('A','T'))
+print(name.upper())
+print(name.lower())
+print('A' in name)
+print(type(name))
+a=10
+print(type(a))
+b=20.33
+print(type(b))
+# convert decimal to binary
+print(bin(16))
+# convert octal to binary
+print(bin(0o15))
+# convert decimal to octal
+print(oct(80))
+# convert binary to octal
+print(oct(0b111))
+# convert decimal to hexadecimal
+print(hex(100))
+# convert octal to hexadecimal
+print(hex(0o555))
+# arithmetic operations 
+print(5+2)
+print(5-2)
+print(5*2)
+print(5/2)
+print(5//2)
+print(5**2)
+i = 5
+i = i+2
+i += 2
+i-=2
+print(i)
+# operator precedence
+result = 2+3*5
+print(result)
+
+
+
+
