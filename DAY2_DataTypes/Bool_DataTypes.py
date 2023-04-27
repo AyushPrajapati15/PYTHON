e61e7c697387d0f22ac464825b7b651f10e3a604
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-04-27 23:57:50 +0530

    DataTypes

---
+++ DAY2_DataTypes/Bool_DataTypes.py
@@ -0,0 +1,30 @@
+# Boolean datatypes
+var=True
+print(var)
+print(type(var))
+
+var1=False
+print(var1)
+print(type(var1))
+
+a = 80
+b = 40
+c = a > b
+print(c)
+
+a = 80
+b = 40
+c = a < b
+print(c)
+
+print(True+True+True+True)
+print(True+True+True+False)
+
+print(True*True)
+print(True*False)
+print(False*True)
+print(False*False)
+
+print(True-True)
+print(True/True)
+print(True%True)
