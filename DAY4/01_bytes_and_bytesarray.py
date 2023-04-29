a8272149a21597efe311bdc78a38ac3827628dee
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-04-29 23:57:25 +0530

    Day-4-Some Arithmetic operations

---
+++ DAY4/01_bytes_and_bytesarray.py
@@ -0,0 +1,56 @@
+# arithmetic operations 
+x = 5
+y = 2
+
+# addition
+z = x + y
+print(z) # Output: 7
+
+# subtraction
+z = x - y
+print(z) # Output: 3
+
+# multiplication
+z = x * y
+print(z) # Output: 10
+
+# division
+z = x / y
+print(z) # Output: 2.5
+
+# floor division (returns the quotient as an integer)
+z = x // y
+print(z) # Output: 2
+
+# modulus (returns the remainder)
+z = x % y
+print(z) # Output: 1
+
+# exponentiation
+z = x ** y
+print(z) 
+
+
+
+# integer division (returns the quotient as an integer, rounds down)
+z = x // y
+print(z) 
+
+# modulus (returns the remainder after division)
+z = x % y
+print(z) 
+
+# exponentiation
+z = x ** y
+print(z) 
+
+# increment and decrement
+x += 1
+y -= 1
+print(x, y)
+
+
+# rounding
+z = round(3.14159, 2)
+print(z) # Output: 3.14
+
