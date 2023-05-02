677d7fdf143f01851a747dd0c88047ef1701ac3b
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-05-02 23:36:25 +0530

    Day-8_Arithmetic,Assignment,relational,logical, operator

---
+++ Day8/01-Arithmetic,Assignment,relational,logical, operator.py
@@ -0,0 +1,36 @@
+# Arithmetic operators
+x = 10
+y = 3
+print("x + y = ", x + y)
+print("x - y = ", x - y)
+print("x * y = ", x * y)
+print("x / y = ", x / y)
+print("x % y = ", x % y)
+print("x // y = ", x // y)
+print("x ** y = ", x ** y)
+
+# Assignment operators
+a = 5
+a += 2
+print("a = ", a)
+a -= 1
+print("a = ", a)
+a *= 3
+print("a = ", a)
+
+# Relational operators
+b = 7
+c = 12
+print("b == c is ", b == c)
+print("b != c is ", b != c)
+print("b < c is ", b < c)
+print("b > c is ", b > c)
+print("b <= c is ", b <= c)
+print("b >= c is ", b >= c)
+
+# Logical operators
+d = 3
+e = 7
+print("d < 5 and e > 5 is ", d < 5 and e > 5)
+print("d < 5 or e > 5 is ", d < 5 or e > 5)
+print("not(d < 5 and e > 5) is ", not(d < 5 and e > 5))
