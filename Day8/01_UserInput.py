d185ccf45f1c759cd9efe01f46f38066f08b918c
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-05-03 20:33:36 +0530

    Day-8_ user input

---
+++ Day8/01_UserInput.py
@@ -0,0 +1,64 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# Arithmetic operators
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+modulus = a % b
+exponentiation = a ** b
+integer_division = a // b
+
+# Bitwise operators
+num=bitwise_and = a & b
+print(num)
+num=bitwise_or = a | b
+print(num)
+num=bitwise_xor = a ^ b
+print(num)
+num=bitwise_left_shift = a << b
+print(num)
+num=bitwise_right_shift = a >> b
+print(num)
+num=bitwise_complement = ~a
+print(num)
+
+# Comparison operators
+num=less_than = a < b
+print(num)
+num=greater_than = a > b
+print(num)
+num=equal_to = a == b
+print(num)
+num=not_equal_to = a != b
+print(num)
+num=less_than_or_equal_to = a <= b
+print(num)
+num=greater_than_or_equal_to = a >= b
+print(num)
+
+# Logical operators
+num=logical_and = a and b
+print(num)
+num=logical_or = a or b
+print(num)
+num=logical_not_a = not a
+print(num)
+num=logical_not_b = not b
+print(num)
+
+# Identity operators
+num=identity_is = a is b
+print(num)
+num=identity_is_not = a is not b
+print(num)
+
+# Display the results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Modulus:", modulus)
+print("Exponentiation:", exponentiation)
+print("integer_division:", integer_division)
