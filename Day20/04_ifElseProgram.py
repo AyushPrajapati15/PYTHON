3f2b9b38646fbcddefaadabbe44bd6f0e7ae7b89
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-05-15 22:57:04 +0530

    Day-20 if else programming questions

---
+++ Day20/04_ifElseProgram.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number: "))
+
+if number % 5 == 0 and number % 11 == 0:
+
+    print("The number is divisible by both 5 and 11.")
+
+else:
+
+    print("The number is not divisible by both 5 and 11.")
