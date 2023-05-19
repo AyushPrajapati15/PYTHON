fe6c1cd76b2f71807ddeed77c1a2bff7627b16cc
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-05-19 23:36:47 +0530

    Day-24 lists method,program practice,

---
+++ Day24/01_ListsMethods.py
@@ -0,0 +1,21 @@
+# Lists methods :-
+
+# Append()
+l=[11,22,33,44,55]
+l.append(123)
+print(l)
+
+# wap to create a list by adding 20 0's
+l = []
+for i in range(20):
+    l.append(0)
+
+print(l)
+
+# insert()
+l=[10,20,30,40,50]
+l.insert(100,2)
+print(l)
+
+l.insert(88,4)
+print(l)
