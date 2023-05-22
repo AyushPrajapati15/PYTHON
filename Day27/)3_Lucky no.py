bdeeda0fd1a92c00e5e238394cee9c3cd9092428
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-05-22 23:57:42 +0530

    Day-27 Concatenation,repeatition,membership opeator, some programs

---
+++ Day27/)3_Lucky no.py
@@ -0,0 +1,10 @@
+l=[1,2,3,5,6,7,9,11,12,17,18,23,24,25,67,92,99]
+ch=int(input("Enter your lucky number"))
+
+if ch in l:
+    print("Yes your lucky number is present in the lst")
+else:
+    print("No your lucky number is not present in the ist")
+
+
+# Wap to check your lucky no is there in the list or not unti
