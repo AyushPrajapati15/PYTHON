6960f18dbca0e554868cc11375943ee97b41d9ee
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-05-23 21:43:36 +0530

    Day-28 Reverse a list

---
+++ Day28/01_Reverse.py
@@ -0,0 +1,16 @@
+# Print list in reverse order using positive index
+l=[10,20,30,40,50]
+n=len(l)-1
+
+while n>=0:
+    print(l[n])
+    n=n-1
+print()
+
+
+# Print list in reverse order using negative index
+l=[10,20,30,40,50]
+i=-1
+while i>= -len(l):
+    print(l[i])
+    i=i-1
