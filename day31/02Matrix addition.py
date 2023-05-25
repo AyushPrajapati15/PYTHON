c535626267fb9b3e4ce34dc35f1aa4076dd849ba
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-05-26 00:59:30 +0530

    Day31-Matrix multiplication,addition,sum of rows and columns

---
+++ day31/02Matrix addition.py
@@ -0,0 +1,20 @@
+# Matrix addition
+matrix1 = [[1, 2, 3],
+           [4, 5, 6],
+           [7, 8, 9]]
+
+matrix2 = [[9, 8, 7],
+           [6, 5, 4],
+           [3, 2, 1]]
+
+result = [[0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]]
+
+for i in range(len(matrix1)):
+    for j in range(len(matrix1[0])):
+        result[i][j] = matrix1[i][j] + matrix2[i][j]
+
+print("Result of matrix addition:")
+for row in result:
+    print(row)
