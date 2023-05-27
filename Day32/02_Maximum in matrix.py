bc3c0e84acbac15624e776dce587a6d72de5c189
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-05-27 21:46:00 +0530

    Day-32 Transpose a matrix,Maximum,Symmetric or not programs

---
+++ Day32/02_Maximum in matrix.py
@@ -0,0 +1,21 @@
+# Maximum element in a matrix
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+
+max_element = matrix[0][0]
+
+print("The current matrix is ")
+
+for i in matrix:
+    print(i)
+
+print()
+
+for i in matrix:
+    for j in i:
+        if j > max_element:
+            max_element = j
+
+print("The maximum element of the matrix is: ")
+print(max_element)
