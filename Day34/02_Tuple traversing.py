80b4f2eb3deb789bc2eb4212a2cd9cb4f62ebdce
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-05-29 22:03:25 +0530

    Day34- Tuple traversing,tuple methods.

---
+++ Day34/02_Tuple traversing.py
@@ -0,0 +1,29 @@
+# Traversing operation on tuple
+# Traverse using for loop
+t=(10,20,30,40,50)
+for i in t:
+    print(i)
+print()
+
+t=(10,20,30,40,50)
+for i in t:
+    print(t)
+print()
+
+# Traverse using while loop
+t=(10,20,30,40,50)
+i=0
+while i<len(t):
+    print(t[i])
+    i=i+1
+print()
+
+t=(10,20,30,40,50)
+i=0
+while i<len(t):
+    print(t)
+    i=i+1
+print()
+
+
+
