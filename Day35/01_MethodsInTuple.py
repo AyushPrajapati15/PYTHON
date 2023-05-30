d14595035facc8f0a339a9b186b66f90717ab189
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-05-30 20:08:59 +0530

    Day-35 Tuple

--- Day35/01_MethodsInTuple.py
+++ Day35/01_MethodsInTuple.py
@@ -13,6 +13,7 @@ my_tuple = ('H', 'e', 'l', 'l', 'o')
 string = ''.join(my_tuple)
 print(string)
 
+
 my_tuple = (1, 2, 3, 4, 5)
 length = len(my_tuple)
 print(f"The length of the tuple is: {length}")
