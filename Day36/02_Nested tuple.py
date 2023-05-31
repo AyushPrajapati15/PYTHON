09ecba7e87cf23cd1d9f33fe1d7bf431a9046d64
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-05-31 23:59:31 +0530

    Day-36 Tuple packing and unpacking,nested tuple

--- Day36/02_Nested tuple.py
+++ Day36/02_Nested tuple.py
@@ -1,39 +1,27 @@
 # Create a nested tuple
 nested_tuple = (('a', 'b', 'c'), ('d', 'e', 'f'), ('g', 'h', 'i'))
 
-print(nested_tuple[0][1])  # Output: b
-
-print(nested_tuple[1][2])  # Output: f
-
-print(nested_tuple[2][0])  # Output: g
+print(nested_tuple[0][1])
+print(nested_tuple[1][2])
+print(nested_tuple[2][0])
 
 
-# Create a nested tuple
+
 nested_tuple = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
-
 nested_tuple[0] = (nested_tuple[0][0], 99, nested_tuple[0][2])
-
 nested_tuple[1] = (nested_tuple[1][0], nested_tuple[1][1], 88)
-
 nested_tuple[2] = (77, nested_tuple[2][1], nested_tuple[2][2])
-
 print(nested_tuple)
 
 
-# Create a nested tuple
-nested_tuple = (('a', 'b', 'c'), ('d', 'a', 'e'), ('f', 'g', 'a'))
-
 
 # Create two nested tuples
-nested_tuple1 = ((1, 2, 3), (4, 5, 6))
-nested_tuple2 = (('a', 'b', 'c'), ('d', 'e', 'f'))
-
-# Concatenate the two nested tuples
-concatenated_tuple = nested_tuple1 + nested_tuple2
-
+tuple1 = ((1, 2, 3), (4, 5, 6))
+tuple2 = (('a', 'b', 'c'), ('d', 'e', 'f'))
+concatenated_tuple = tuple1 + tuple2
 print(concatenated_tuple)
 
-# Create a nested tuple
+
 nested_tuple = ((1, 2, 3), ('a', 'b', 'c'), (4, 5, 6))
 
 if 'b' in nested_tuple:
