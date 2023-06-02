d1cf673e89bb582aa71b5a33ac2f350da96f1de7
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-06-02 22:49:05 +0530

    Day-38 set methods,mathematical operators in set

--- Day38/02_set methods(Mathematical).py
+++ Day38/02_set methods(Mathematical).py
@@ -5,6 +5,7 @@ print(a.union(b))
 print(a|b)
 print()
 
+
 # intersection()
 a={10,20,30,40,50}
 b={11,22,33,44,10,20}
@@ -12,12 +13,14 @@ print(a.intersection(b))
 print(a&b)
 print()
 
+
 # difference()
 a={10,20,30,40,50}
 b={11,22,33,44,10,20}
 print(a.difference(b))
 print(a-b)
 print()
+
 
 # symmetric_difference()
 a={10,20,30,40,50}
@@ -33,6 +36,7 @@ b={5,6,7}
 print(b.issubset(a))
 print(a.issubset(b))
 print()
+
 
 # superset
 a={1,2,3,4,5,6,7,8,9,}
