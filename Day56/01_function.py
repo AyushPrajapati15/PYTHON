0b3d65e6c752645735c38aef5f18c8e24d72f55d
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-06-20 23:17:53 +0530

    Day-56 Introduction to Functions

---
+++ Day56/01_function.py
@@ -0,0 +1,47 @@
+# check the number is even or odd (without using function)
+a = 10
+if a % 2 == 0:
+    print('even')
+else:
+    print('odd')
+
+b = 20
+if b % 2 == 0:
+    print('even')
+else:
+    print('odd')
+
+
+c = 89
+if c % 2 == 0:
+    print('even')
+else:
+    print('odd')
+
+
+d = 90
+if d % 2 == 0:
+    print('even')
+else:
+    print('odd')
+
+
+e = 1023
+if e % 2 == 0:
+    print('even')
+else:
+    print('odd')
+
+# Check the number is even or odd 
+def evenodd(num):
+    if num%2==0:
+        print("Even")
+    else:
+        print("Odd")
+
+evenodd(10)
+evenodd(25)
+evenodd(55)
+evenodd(33)
+evenodd(44)
+evenodd(64)
