46a625e9798c2ac3a4e4f11ecc72ded87dc69be8
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-06-23 09:11:20 +0530

    Day-59 call by value,reference,object

--- Day59/01_Call by value_reference_object.py
+++ Day59/01_Call by value_reference_object.py
@@ -1,6 +1,7 @@
 def fun(a):
     print('inside fun', a)
     print('inside fun ', id(a))
+    print()
 
 
 a = 23
@@ -8,11 +9,12 @@ print('outside fun', a)
 fun(a)
 print('outside fun', a)
 print('outside fun ', id(a))
-
+print()
 
 def fun(x):
     print('inside fun', x)
     print('inside fun ', id(x))
+    print()
 
 
 a = 23
@@ -20,6 +22,7 @@ print('outside fun', a)
 fun(a)
 print('outside fun', a)
 print('outside fun ', id(a))
+print()
 
 
 # call by value
@@ -27,17 +30,21 @@ print('outside fun ', id(a))
 def fun(a):
     print('inside fun before modification  : ', a)
     print('inside fun before modification  : ', id(a))
+    print()
     a = 1000
     print('inside fun after modification  : ', a)
     print('inside fun after modification  : ', id(a))
+    print()
 
 
 a = 23
 print('outside fun before calling  : ', a)
 print('outside fun before calling: ', id(a))
+print()
 fun(a)
 print('outside fun after calling: ', a)
 print('outside fun after calling: ', id(a))
+print()
 
 
 # call by referance
@@ -45,14 +52,18 @@ print('outside fun after calling: ', id(a))
 def fun(l):
     print('inside fun ', l)
     print('inside fun ', id(l))
+    print()
     l.append(1000)
     print('inside fun ', l)
     print('inside fun ', id(l))
+    print()
 
 
 l = [23, 33, 43]
 print('outside fun ', l)
 print('outside fun ', id(l))
+print()
 fun(l)
 print('outside fun ', l)
 print('outside fun ', id(l))
+print()
