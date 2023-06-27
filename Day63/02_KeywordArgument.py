846fde4a155ab3e83e7cf28284ed46d33d935099
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-06-27 22:51:39 +0530

    Day-63 Positional, Keyword Argument

--- Day63/02_KeywordArgument.py
+++ Day63/02_KeywordArgument.py
@@ -5,6 +5,7 @@ cal(a=5, b=6, c=7)  # 47
 cal(5,6,c=7)
 cal(5, b=6, c=7)
 cal(5, c=6, b=7)
+print()
 # cal(5, a=6, b=7)  # TypeError: cal() got multiple values for argument 'a'
 # cal(5, 6, b=7)  # TypeError: cal() got multiple values for argument 'b'
 
@@ -25,13 +26,13 @@ cal(5, 6, 7)
 
 def cal(a, b, c):
     print(a+b*c)
-
+print()
 # cal(6,a=7,5)
 
 
 def cal(a, b, c, d, e, f):
     print(a+b*c)
-
+print()
 
 # cal(6, 7, 8, e=9, 10, 11)
 # cal(6, 7, f=8, 10, 11, a=9)
