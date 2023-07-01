dc101ff5ad11126956821e0884f3b1189b10f220
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-01 20:30:50 +0530

    Day-67 * and * function call

--- Day67/01_Single and double tar function call.py
+++ Day67/01_Single and double tar function call.py
@@ -7,7 +7,7 @@ def fun(a, x, y, z):
     print(z)
 
 fun(10, 20, 30, 40)
-
+print()
 
 def fun(a, x, y, z):
     print(a)
@@ -17,7 +17,7 @@ def fun(a, x, y, z):
 
 l = [10, 20, 30, 40]
 fun(*l)
-
+print()
 
 def fun(a, x, y, z):
     print(a)
@@ -28,7 +28,7 @@ def fun(a, x, y, z):
 l = [10, 20, 30]
 # fun(*l)
 fun(55,*l)
-
+print()
 
 def fun(a, x, y, z):
     print(a)
@@ -38,7 +38,7 @@ def fun(a, x, y, z):
 
 l = [10, 20, 30]
 fun(*l, 777)
-
+print()
 
 def fun(a, x, y, z):
     print(a)
@@ -48,7 +48,7 @@ def fun(a, x, y, z):
 
 l = [10, 20, 30]
 fun(777, *l)
-
+print()
 
 def fun(a, x, y, z):
     print(a)
@@ -59,7 +59,7 @@ def fun(a, x, y, z):
 l = [10, 20]
 # fun(777, *l)
 fun(777, *l,55)
-
+print()
 
 def fun(a, x, y, z):
     print(a)
@@ -69,7 +69,7 @@ def fun(a, x, y, z):
 
 l = [10, 20]
 fun(777, 888, *l)
-
+print()
 
 def fun(a, x, y, z):
     print(a)
@@ -80,7 +80,7 @@ def fun(a, x, y, z):
 l = [10, 20, 30]
 # fun(777, 888, *l)
 fun(12,*l)
-
+print()
 
 def fun(a, x, y, z):
     print(a)
@@ -90,7 +90,7 @@ def fun(a, x, y, z):
 
 t = (10, 20, 30, 40)
 fun(*t)
-
+print()
 
 def fun(a, x, y, z):
     print(a)
@@ -100,7 +100,7 @@ def fun(a, x, y, z):
 
 s = {10, 20, 30, 40}
 fun(*s)
-
+print()
 
 # ** in function call
 def fun(a, **s):
@@ -108,7 +108,7 @@ def fun(a, **s):
     print(s)
 
 fun(10, i=100)
-
+print()
 
 def fun(a, **s):
     print(a)
@@ -116,7 +116,7 @@ def fun(a, **s):
 
 d = {'i': 100, 'j': 200, 'k': 300}
 fun(10, **d)
-
+print()
 
 def fun(a, **s):
     print(a)
