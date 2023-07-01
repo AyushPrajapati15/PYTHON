a34a59a168581f12e696326292e427f525057e79
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-01 20:27:30 +0530

    Day-67 * and * function call

---
+++ Day67/01_Single and double tar function call.py
@@ -0,0 +1,149 @@
+# Single(*) and double star(*) function call
+
+def fun(a, x, y, z):
+    print(a)
+    print(x)
+    print(y)
+    print(z)
+
+
+fun(10, 20, 30, 40)
+
+
+def fun(a, x, y, z):
+    print(a)
+    print(x)
+    print(y)
+    print(z)
+
+
+l = [10, 20, 30, 40]
+
+fun(*l)
+
+
+def fun(a, x, y, z):
+    print(a)
+    print(x)
+    print(y)
+    print(z)
+
+
+l = [10, 20, 30]
+
+# fun(*l)
+fun(55,*l)
+
+
+def fun(a, x, y, z):
+    print(a)
+    print(x)
+    print(y)
+    print(z)
+
+
+l = [10, 20, 30]
+
+fun(*l, 777)
+
+
+def fun(a, x, y, z):
+    print(a)
+    print(x)
+    print(y)
+    print(z)
+
+
+l = [10, 20, 30]
+
+fun(777, *l)
+
+
+def fun(a, x, y, z):
+    print(a)
+    print(x)
+    print(y)
+    print(z)
+
+
+l = [10, 20]
+
+# fun(777, *l)
+fun(777, *l,55)
+
+
+def fun(a, x, y, z):
+    print(a)
+    print(x)
+    print(y)
+    print(z)
+
+
+l = [10, 20]
+
+fun(777, 888, *l)
+
+
+def fun(a, x, y, z):
+    print(a)
+    print(x)
+    print(y)
+    print(z)
+
+
+l = [10, 20, 30]
+
+# fun(777, 888, *l)
+fun(12,*l)
+
+
+def fun(a, x, y, z):
+    print(a)
+    print(x)
+    print(y)
+    print(z)
+
+
+t = (10, 20, 30, 40)
+
+fun(*t)
+
+
+def fun(a, x, y, z):
+    print(a)
+    print(x)
+    print(y)
+    print(z)
+
+
+s = {10, 20, 30, 40}
+
+fun(*s)
+
+
+# ** in function call
+
+def fun(a, **s):
+    print(a)
+    print(s)
+
+
+fun(10, i=100)
+
+
+def fun(a, **s):
+    print(a)
+    print(s)
+
+
+d = {'i': 100, 'j': 200, 'k': 300}
+fun(10, **d)
+
+
+def fun(a, **s):
+    print(a)
+    print(s)
+
+
+d = {'i': 100, 'j': 200, 'k': 300}
+fun(10, i=100,j=200,k=300)
