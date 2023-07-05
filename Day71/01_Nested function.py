ada3636b9c120d7318bacca45f0f19278fb55a64
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-05 22:19:49 +0530

    Day-71 nested function, Function returning another function

---
+++ Day71/01_Nested function.py
@@ -0,0 +1,36 @@
+# nested function
+
+def outer_fun():
+    print('i m inside outer fun')
+
+    def inner_fun():
+        print('i m inside inner fun')
+    inner_fun()
+
+outer_fun()
+
+
+def outer_fun():
+    print('i m inside outer fun')
+
+    def inner_fun():
+        print('i m inside inner fun')
+
+
+outer_fun()
+# inner_fun()
+
+
+def outer_fun():
+    print('i m inside outer fun')
+
+    def inner_fun():
+        print('i m inside inner fun')
+    inner_fun()
+
+outer_fun()
+# inner_fun()
+
+
+
+
