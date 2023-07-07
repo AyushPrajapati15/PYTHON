4ec5dd23fab25796ee0bbea5791a831cb5842ce7
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-07 23:24:37 +0530

    Day-73 Function returning another function

---
+++ Day73/01_Function returning another function.py
@@ -0,0 +1,67 @@
+# Function returning another function
+
+# function called multiply_by that takes a number n as input and returns a function that multiplies its argument by n. Test the function by creating a multiplier function and using it to multiply a given number.
+def multiply_by(n):
+    def multiplier(x):
+        return x * n
+    return multiplier
+
+multiplier_2 = multiply_by(2)
+result = multiplier_2(5)
+print(result)
+
+
+
+def get_math_operation(operation):
+    if operation == 'add':
+        return lambda x, y: x + y
+    elif operation == 'subtract':
+        return lambda x, y: x - y
+    elif operation == 'multiply':
+        return lambda x, y: x * y
+    elif operation == 'divide':
+        return lambda x, y: x / y
+
+addition = get_math_operation('add')
+result = addition(5, 3)
+print(result)
+
+multiplication = get_math_operation('multiply')
+result = multiplication(4, 2)
+print(result)
+
+
+def apply_discount(percent):
+    def discount(price):
+        return price - (price * percent / 100)
+    return discount
+
+ten_percent_discount = apply_discount(10)
+final_price = ten_percent_discount(100)
+print(final_price)
+
+
+def power_of(n):
+    def power(x):
+        return x ** n
+    return power
+
+
+square = power_of(2)
+result = square(5)
+print(result)
+
+cube = power_of(3)
+result = cube(2)
+print(result)
+
+
+def create_incrementor(n):
+    def incrementor(x):
+        return x + n
+    return incrementor
+
+
+increment_by_3 = create_incrementor(3)
+result = increment_by_3(5)
+print(result)
