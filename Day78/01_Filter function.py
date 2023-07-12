b1af422c5c3b45778aa5a30dd159b65554469be4
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-12 20:36:16 +0530

    Day-78 Filter function

--- Day78/01_Filter function.py
+++ Day78/01_Filter function.py
@@ -10,33 +10,45 @@ def checkEven(x):
 l=[10,20,21,13,15,16,18,67,51]
 data=list(filter(checkEven,l))
 print(data)
+print()
+
 
 #write a program to filter all the even value from a list with using lambda
 l=[10,20,21,11,33,17,89,56,19,50]
 data=list(filter(lambda x:x%2==0,l))
 print(data)
+print()
 
  
 l=[10,20,21,33,17,89,56,19]
 data=tuple(filter(lambda x:x%2==0,l))
 print(data)
+print()
+
 
 l=[10,20,21,33,17,89,56,19]
 data=set(filter(lambda x:x%2==0,l))
 print(data)
+print()
+
 
 #write a program to filter all the even value from a list with using lambda in single 
 print(list(filter(lambda x:x%2==0,[10,20,33,12,32,89,76])))
+print()
 
 print(tuple(filter(lambda x:x%2==0,[10,20,33,12,32,89,76])))
+print()
 
 print(list(filter(lambda x:x%2!=0,[10,20,33,12,32,89,76,111,231])))
+print()
 
 #filter all the negative from a sequence using filter 
 l=[10,20,-39,49,-87,-65,-31]
 data=list(filter(lambda x:x<0,l))
 print(data)
 # print(list(filter(lambda x:x<0,l)))
+print()
+
 
 # filter out all the common element from both list using filter() function 
 a=[10,20,30,40,50]
@@ -44,15 +56,21 @@ l=[100,200,30,40,70]
 
 data=list(filter(lambda x:x in a,l))
 print(data)
+print()
+
 
 # filter only one name Bob from a list uisng filter function
 names=['Alice','Bob','Jack','Jerry','Bob']
 data=list(filter(lambda x:x=='David',names))
 print(data)
+print()
+
 
 names=['Alice','Bob','Jack','Jerry','Bob']
 data=list(filter(lambda x:x!='David',names))
 print(data)
+print()
+
 
 d={
     1:'Alice',
@@ -65,6 +83,8 @@ d={
 
 data=dict(filter(lambda x:x[0]%2==0,d.items()))
 print(data)
+print()
+
 
 d={
     1:'Alice',
