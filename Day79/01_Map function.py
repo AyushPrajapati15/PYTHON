b838604c305e40482a66403f4eab7fe0206289ea
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-13 20:37:01 +0530

    Day-79 Map function

--- Day79/01_Map function.py
+++ Day79/01_Map function.py
@@ -6,10 +6,12 @@ l=[10,20,30,40,50]
 data=list(map(add4,l))
 print(data)
  
+
 # add 4 to each and every element present in list with using lambda 
 l=[100,200,300,400,500]
 data=list(map(lambda x:x+4,l))
 print(data)
+
 
 #find out square of ecah number preset inside tuple 
 t=(1,2,3,4,5,6)
@@ -18,12 +20,13 @@ data=list(map(lambda x:x*x,t))
 print(data)
 print(data1)
 
+
 #add individual elemenet of both list
 l1=[10,20,30,40,50]
 l2=[11,12,13,14,15]
-
 data=list(map(lambda x,y:x+y,l1,l2))
 print(data)
+
 
 l1=[10,20,30,40,50]
 l2=[11,12,13,14,15]
@@ -31,19 +34,21 @@ l3=[111,112,113,114,115]
 data=list(map(lambda x,y,z:x+y+z,l1,l2,l3))
 print(data)
 
+
 #find the length of each words preset in a list 
-
 names=['Alice','Bob','Jack','Marry']
-
 data=list(map(lambda x:len(x),names))
 print(data)
 
 
 names=['Alice','Bob','Jack','zini']
 data=map(lambda x:len(x),names)
+
+
 # print(type(data))
 for i in data:
     print(i)
+
     
 # find out the length of each words preset in a string 
 msg='hello everyone welcome to python'
