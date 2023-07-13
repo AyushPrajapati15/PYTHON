90fc1583f71f4219c237c71c8710e308197c9af1
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-13 20:38:36 +0530

    Day-79 Map function

--- Day79/01_Map function.py
+++ Day79/01_Map function.py
@@ -5,12 +5,14 @@ def add4(x):
 l=[10,20,30,40,50]
 data=list(map(add4,l))
 print(data)
- 
+print()
+
 
 # add 4 to each and every element present in list with using lambda 
 l=[100,200,300,400,500]
 data=list(map(lambda x:x+4,l))
 print(data)
+print()
 
 
 #find out square of ecah number preset inside tuple 
@@ -19,6 +21,7 @@ data1=tuple(map(lambda x:x*x,t))
 data=list(map(lambda x:x*x,t))
 print(data)
 print(data1)
+print()
 
 
 #add individual elemenet of both list
@@ -26,6 +29,7 @@ l1=[10,20,30,40,50]
 l2=[11,12,13,14,15]
 data=list(map(lambda x,y:x+y,l1,l2))
 print(data)
+print()
 
 
 l1=[10,20,30,40,50]
@@ -33,21 +37,25 @@ l2=[11,12,13,14,15]
 l3=[111,112,113,114,115]
 data=list(map(lambda x,y,z:x+y+z,l1,l2,l3))
 print(data)
+print()
 
 
 #find the length of each words preset in a list 
 names=['Alice','Bob','Jack','Marry']
 data=list(map(lambda x:len(x),names))
 print(data)
+print()
 
 
 names=['Alice','Bob','Jack','zini']
 data=map(lambda x:len(x),names)
+print()
 
 
 # print(type(data))
 for i in data:
     print(i)
+print()
 
     
 # find out the length of each words preset in a string 
