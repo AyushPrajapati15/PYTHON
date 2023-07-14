4f37a0edd64d0dc66bb39898a5079053841f4dea
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-14 20:26:11 +0530

    Day-80 Reduce function

---
+++ Day80/01_Reduce Function.py
@@ -0,0 +1,36 @@
+#find the sum of all element present inside list without using lambda 
+from functools import reduce
+def add(x,y):
+    return x+y
+
+l=[1,2,3,4,5]
+data=reduce(add,l)
+print(data)
+
+def add(x,y):
+    return x*y
+
+l=[1,2,3,4,5]
+data=reduce(add,l)
+print(data)
+
+#find the sum of all element present inside list with using lambda 
+l1=[1,2,3,4,5]
+data=reduce(lambda x,y:x+y,l1)
+print(data)
+print(type(data))
+
+#find the product of all element present inside list with using lambda 
+l1=[1,2,3,4,5]
+data=reduce(lambda x,y:x*y,l1,10)
+print(data)
+
+# find out the sum of all the elements from 1 to 100
+data=reduce(lambda x,y:x+y,range(1,101))
+print(data)
+
+#working with list which is containg string data 
+names=['surendra','priyanka','rahul','zini']
+data=reduce(lambda x,y:x+y,names,'hello')
+print(data)
+print(type(data))
