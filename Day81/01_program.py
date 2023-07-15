089bdf0753fd6e025b7aef68173422eb91c5bc41
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-15 22:56:18 +0530

    Day-81 Filter map reduce programs

--- Day81/01_program.py
+++ Day81/01_program.py
@@ -4,6 +4,7 @@ from functools import reduce
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
 print(even_numbers)
+print()
 
 
 names = ["Alice", "Bob", "Charlie", "Dave"]
@@ -16,34 +17,39 @@ print()
 numbers = [1, 2, 3, 4, 5]
 product = reduce(lambda x, y: x * y, numbers)
 print(product)
+print()
 
 
 strings = ["apple", "banana", "carrot", "date", "eggplant"]
 filtered_strings = list(filter(lambda x: len(x) > 5, strings))
 print(filtered_strings)
+print()
 
 
 
 numbers = [1, 2, 3, 4, 5]
 squared_numbers = list(map(lambda x: x ** 2, numbers))
 print(squared_numbers)
+print()
 
 
 strings = ["apple", "banana", "carrot", "date", "eggplant"]
 filtered_strings = list(filter(lambda x: x[0].lower() in ['a', 'e', 'i', 'o', 'u'], strings))
 print(filtered_strings)
-
+print()
 
 
 
 names = ["Alice", "Bob", "David", "Mary"]
 concatenated_names = list(reduce(lambda x, y: x + " " + y, names))
 print(concatenated_names)
+print()
 
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 divisible_by_3_numbers = list(filter(lambda x: x % 3 == 0, numbers))
 print(divisible_by_3_numbers)
+print()
 
 
 strings = ["apple", "banana", "carrot", "date", "eggplant"]
