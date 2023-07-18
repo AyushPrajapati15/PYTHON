f8cf4260afd4f7656ff1e35caa54a1c865ec7881
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-18 19:58:43 +0530

    Day-84 Modules Concept

---
+++ Day84/01_cal.py
@@ -0,0 +1,37 @@
+import hello  # here we are importing hello modules  that means we can access all the mebers of hello modules 
+print(hello.add(10,20))
+print(hello.sub(10,20))
+print(hello.mul(10,20))
+print(hello.div(10,20))
+
+
+
+import math
+print(math.factorial(5))
+print(math.factorial(6))
+120
+720
+
+#module alish
+import hello as h
+print(h.add(100,1000))
+print(h.pi)
+print(hello.sub(1000,20))
+
+#Different ways of imporing modules
+from hello import add ,sub,mul
+print(add(100,29))
+print(sub(100,20))
+print(mul(10,20))
+
+from hello import *
+print(add(10,30))
+print(sub(10,20))
+print(pi)
+
+
+#module member alishing 
+from hello import add as a ,sub as s , pi as p 
+print(a(10,1))
+print(pi)
+print(p)
