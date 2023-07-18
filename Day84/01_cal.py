c25a4bb25ef9333215d43181dd89871fe636f9e4
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-18 20:00:46 +0530

    Day-84 Modules Concept

--- Day84/01_cal.py
+++ Day84/01_cal.py
@@ -11,6 +11,7 @@ print(math.factorial(5))
 print(math.factorial(6))
 print()
 
+
 #module alish
 import hello as h
 print(h.add(100,1000))
@@ -18,17 +19,20 @@ print(h.pi)
 print(hello.sub(1000,20))
 print()
 
+
 #Different ways of imporing modules
 from hello import add ,sub,mul
 print(add(100,29))
 print(sub(100,20))
 print(mul(10,20))
 print()
+
 from hello import *
 print(add(10,30))
 print(sub(10,20))
 print(pi)
 print()
+
 
 #module member alishing 
 from hello import add as a ,sub as s , pi as p 
