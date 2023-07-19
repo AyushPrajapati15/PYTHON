500b7180a31f980c81cf2a29382439eea0cfe24f
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-19 22:39:10 +0530

    Day-85 dir(),help(),__name__, "__main__"

---
+++ Day85/module.py
@@ -0,0 +1,23 @@
+# Creating module
+
+pi = 3.14
+
+
+def add(a, b):
+    return a+b
+
+
+def sub(a, b):
+    return a-b
+
+
+def mul(a, b):
+    return a*b
+
+
+def div(a, b):
+    return a/b
+
+
+# print(add(10,20))
+print(__name__)
