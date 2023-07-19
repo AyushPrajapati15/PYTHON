53db05bd67038f615b77b2c62ede7beeab41abd3
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-19 22:38:56 +0530

    Day-85 dir(),help(),__name__, "__main__"

---
+++ Day85/01_dir_help.py
@@ -0,0 +1,20 @@
+# dir() vs help()
+import math
+import os
+import module
+print("dir()")
+print(dir(math))
+print()
+print(dir(os))
+print()
+print(dir(module))
+print()
+# dir() function will  give only the members name
+
+print("dir()")
+# help() function is used to give you all the members name along with description
+# print(help(math))
+print()
+# print(help(os))
+print()
+print(help(module))
