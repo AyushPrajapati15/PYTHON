ca61aef49b6c301ed69f6b8cbf421dc1f9995579
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-20 22:52:29 +0530

    Day-86 MathModule

--- Day86/01_mathModule.py
+++ Day86/01_mathModule.py
@@ -2,13 +2,16 @@
 import math
 # print(help(math))
 print(dir(math))
+print()
 
 
 # area of circle
 area = math.pi*3.9*3.9
 print(area)
+print()
 
 print(math.e)
+print()
 
 
 # working with different function
@@ -16,26 +19,33 @@ print(math.ceil(2.1))
 print(math.ceil(2.2))
 print(math.ceil(2.9))
 print(math.ceil(3))
+print()
 
 print(math.floor(2.1))
 print(math.floor(2.2))
 print(math.floor(2.7))
 print(math.floor(2.9))
 print(math.floor(3))
+print()
 
 print(math.sqrt(144))
+print()
 
 print(math.floor(math.sqrt(144)))
+print()
 
 print(math.factorial(6))
+print()
 
 print(math.fabs(-2))
 print(math.fabs(2))
 print(math.fabs(-3.5))
 print(math.fabs(-10))
 print(math.fabs(10))
+print()
 
 print(math.floor(math.fmod(10, 2)))
-print(math.floor(math.fmod(10, 3)))
+print(math.fmod(10,2))
+print()
 
 print(math.floor(math.fsum([10, 20, 30, 40, 50])))
