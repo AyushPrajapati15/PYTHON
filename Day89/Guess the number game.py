f0546b0e0649ec5947fa57743307caa7f817ee7b
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-23 23:35:38 +0530

    Day-88 Guess the number game

---
+++ Day89/Guess the number game.py
@@ -0,0 +1,26 @@
+# Guess the number Game
+
+import random
+
+rn=random.randint(1,10)
+
+count=0
+
+while(True):
+
+    choice=int(input("Guess a number in between 1 - 10 \n"))
+    print()
+
+    count=count+1
+
+    if choice==rn:
+        print(f"Congrats! you guess the correct number in {count} times")
+        print()
+        break
+    elif choice>rn:
+        print("Sry! your gues number is greater than the number")
+        print()
+    else:
+        print("Sry! your gues number is less than the number")
+        print()
+
