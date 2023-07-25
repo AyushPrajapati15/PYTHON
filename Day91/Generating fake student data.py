ed70d67c5f986b52544591b771ca1045f78f2aba
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-25 18:45:07 +0530

    Day-91 Generating fake student data without faker module

--- Day91/Generating fake student data.py
+++ Day91/Generating fake student data.py
@@ -4,13 +4,14 @@
 from random import *
 alpha="abcdefghijklmnopqrstuvwxyz"
 
-for i in range(20):
+for i in range(10):
     fname=choice(alpha).upper()
     lname=choice(alpha).upper()
     for i in range(randint(3,13)):
         fname=fname+choice(alpha)
         lname=lname+choice(alpha)
     print(fname,lname)
+print()
 
 #studnet mobile no 
 digits="0123456789"
@@ -20,22 +21,26 @@ for i in range(20):
     for i in range(9):
         smob=smob+choice(digits)
     print(smob)
+print()
 
-#sage
+#s_age
 for i in range(20):
     sage=randint(16,25)
     print(sage)
-    
+print()
+
 cities=['bbsr','cuttack','hyd','delhi','puri']
 for i in range(20):
     saddr=choice(cities)
     print(saddr)
-    
+print()
+  
 course=['Btech','MCA','MBA','MTECH','PHD','MSC','MA']
 for i in range(20):
     scourse=choice(course)
     print(scourse)
-    
+print()
+   
 #sid 
 digits="0123456789"
 
