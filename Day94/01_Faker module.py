c911400af006aaa358f39e53953deabb89aaafc1
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-28 19:33:30 +0530

    Day-94  Faker module

---
+++ Day94/01_Faker module.py
@@ -0,0 +1,65 @@
+from faker import Faker  # facker is a module, but Facker is a clss in faker-module
+fk = Faker()
+
+
+print(fk.name())     # fake name
+print()
+print(fk.email())    # fake email
+print()
+print(fk.address())  # fake address
+print()
+print(fk.country())  # fake country
+print()
+print(fk.url())      # fake url
+print()
+print(fk.text())      # fake text
+print()
+print(fk.first_name())   # fake first_name
+print()
+print(fk.last_name())   # fake last_name
+print()
+print(fk.am_pm())   # fake am pm
+print()
+print(fk.boolean())   # fake boolean
+print()
+print(fk.city())   # fake city
+print()
+print(fk.century())   # fake century
+print()
+print(fk.credit_card_number())   # fake credit_card_number
+print()
+print(fk.color())   # fake color
+print()
+print(fk.color_name())   # fake color_name
+print()
+print(fk.company())   # fake company
+print()
+print(fk.company_email()) # fake company_email
+print()
+print(fk.csv())   # fake csv
+print()
+print(fk.json())   # fake json
+print()
+print(fk.currency())   # fake csv
+print()
+print(fk.date())   # fake csv
+print()
+print(fk.date_of_birth())   # fake csv
+print()
+print(fk.domain_name())   # fake csv
+print()
+print(fk.file_extension())   # fake csv
+print()
+print(fk.hex_color())   # fake csv
+print()
+print(fk.hostname())   # fake csv
+print()
+print(fk.free_email())   # fake csv
+print()
+print(fk.hostname())   # fake csv
+print()
+print(fk.ipv4_private())   # fake csv
+print()
+print(fk.job())   # fake csv
+print()
+
