ba34d6db3848e4436e356d087ae7cb7a6f4f4648
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-30 22:52:33 +0530

    Day-95 packages in pythony

---
+++ Day96/program.py
@@ -0,0 +1,24 @@
+# package in python
+
+import university.info
+university.info.getinfo()
+print()
+
+import university.students.bca
+university.students.bca.bca_students()
+print(university.students.bca.noofstudent)
+print()
+
+import university.students.mca
+university.students.mca.mca_students()
+print(university.students.mca.noofstudent)
+print()
+
+import university.teachers.nonteaching
+university.teachers.nonteaching.non_teaching_teachers()
+print(university.teachers.nonteaching.noofteachers)
+print()
+
+import university.teachers.teaching
+university.teachers.teaching.teaching_teachers()
+print(university.teachers.teaching.noofteachers)
